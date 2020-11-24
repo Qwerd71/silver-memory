54f21b6ea439b3458ecb6986f3771e323ef79090
Sabrina <74376638+Sabrina1304@users.noreply.github.com>
2020-11-24 23:15:47 +0100

    Test_Health_bar
    Checker avant de merger, bug portée de l'elephant

---
+++ Unity/silver-memory/Assets/Sab's testing stuff/Player_Test_HealthBar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Test_HealthBar : MonoBehaviour
+{
+    Animator m_Damage;
+
+    public int maxHealth = 5;
+    public int currentHealth;
+
+    public HealthbarScript healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+
+        m_Damage = gameObject.GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(1);
+            m_Damage.SetTrigger("Hit");
+
+            
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
